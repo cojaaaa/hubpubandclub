ed4a3af6202220810e6758ad00b72adb7615dc39
Coja <jovanjovanjovanovic1999@gmail.com>
2022-02-17 18:59:18 +0100

    konekcija

--- koneckija.php
+++ koneckija.php
@@ -1,3 +1,47 @@
 <?php
 
+class KreiranjeTabela{
 
+    private $connection;
+
+    function __construct()
+    {
+        $this->connection = new mysqli('localhost', 'root', '');
+        if ($this->connection->error) {
+         die("Greska pri povezivanju: $this->connection->error");
+        }
+        $this->connection->query("CREATE DATABASE IF NOT EXISTS `hub`"); 
+        $this->connection->select_db('hub');
+        $this->connection->query("CREATE TABLE IF NOT EXISTS jela_pica(
+            ID int AUTO_INCREMENT,
+                name text not null,
+                price int(10) not null,
+                description text null,
+                type text not null,
+                jelo boolean not null,
+                PRIMARY KEY(ID)
+            
+            )");
+        $this->connection->query("CREATE TABLE IF NOT EXISTS users(
+            ID int AUTO_INCREMENT,
+                username text not null,
+                password int(10) not null,
+                email text null,
+                admin boolean not null,
+                PRIMARY KEY(ID)
+            
+            )");   
+        $this->connection->query("CREATE TABLE IF NOT EXISTS rezervacije(
+            ID int AUTO_INCREMENT,
+                ime text not null,
+                prezime text not null,
+                datum DATE not null,
+                vreme text not null,
+                telefon text not null,
+                napomena text null,
+                PRIMARY KEY(ID)
+            
+            )");     
+    }
+}
+$tabele = new KreiranjeTabela;
